688ee836fbcfcdbeb5f9bf5295bbb078dcc853ca
Mohamed Amara <60475574+mohamedamara7@users.noreply.github.com>
2022-05-15 18:59:36 +0200

    team revision

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 
 const CommentSchema = mongoose.Schema({
-  user_id: { type: String },
-  avatar: { type: String },
-  username: { type: String },
-  comment: { type: String },
-  date: { type: Date, default: Date.now },
+  user_id: { type: String, required: true },
+  avatar: { type: String, required: true },
+  username: { type: String, required: true },
+  comment: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
 });
 
 const LikeSchema = mongoose.Schema({
-  user_id: { type: String },
-  like: { type: Boolean },
+  user_id: { type: String, required: true },
+  like: { type: Boolean, required: true }, // true means like, false means dislike
 });
 
 const PostSchema = new mongoose.Schema(
   {
-    writer_id:{type:String , required:true , unique:true},
+    writer_id: { type: String, required: true, unique: true },
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     content: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
-    reviews: [CommentSchema],
+    comments: [CommentSchema],
     likes: [LikeSchema],
+    numberOfLikes: { type: Number, default: 0 },
+    numberOfDisLikes: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
