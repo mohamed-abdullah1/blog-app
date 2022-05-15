3cdfa14abd6ebf6027e2359e0ab23fdf855250d7
khaled1150 <68972788+khaled1150@users.noreply.github.com>
2022-05-15 15:39:58 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
+const postRoute = require("./routes/post");
+//const categoryRoute = require("./routes/category");
+const pinnedRoute = require("./routes/pinned");
 const stripeRoute = require("./routes/stripe");
-const favoriteRoute = require("./routes/favorite");
 const newsRouter = require("./routes/news");
 const cors = require("cors");
 
@@ -16,7 +15,7 @@ dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
+  .then( console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
@@ -25,11 +24,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
+app.use("/api/posts", postRoute);
+//app.use("/api/category", categoryRoute);
+app.use("/api/pinned", pinnedRoute);
 app.use("/api/checkout", stripeRoute);
-app.use("/api/favorites", favoriteRoute);
 app.use("/api/news", newsRouter);
 
 app.listen(process.env.PORT || 5000, () => {
