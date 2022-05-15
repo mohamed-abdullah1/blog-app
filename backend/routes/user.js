e3750a0045a5e2fd9059c885dc7bc9d3a2a85979
khaled1150 <68972788+khaled1150@users.noreply.github.com>
2022-05-15 15:41:44 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,7 @@ const {
 } = require("./verifyToken");
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
-const refreshdb = require("../models/refresh");
+//const refreshdb = require("../models/refresh");
 const CryptoJS = require("crypto-js");
 //UPDATE
 router.put("/:id", verifyToken, async (req, res) => {
@@ -91,41 +91,41 @@ router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.get("/refresh", async (req, res) => {
-  const tok = req.headers.token.split(" ")[1];
-  if (!tok) return res.status(400).json("you are not allowed");
-  try {
-    const tic = await refreshdb.findOne({ token: tok });
-    if (!tic) {
-      return res.status(403).json("this refresh  token has been stopped ...");
-    }
-    jwt.verify(tok, process.env.jwt_refresh, (err, user) => {
-      if (err) return res.status(403).json("invalid token");
-      req.user = user;
-    });
-    const { iat, ...OTHERS } = req.user;
-    const newtoken = jwt.sign(OTHERS, process.env.JWT_SEC, { expiresIn: "3d" });
-    res.status(200).json(newtoken);
-  } catch (err) {
-    res.status(403).json("hellooooo");
-  }
-});
-// in refresh
-// headers ---> refresh token and return newAccessToken
-router.delete(
-  "/deleterefresh/:tok",
-  verifyTokenAndAuthorization,
-  async (req, res) => {
-    const tok = req.params.tok;
-    if (!tok) return res.status(400).json("you are not allowed");
-    try {
-      await refreshdb.findOneAndDelete({ token: tok });
-      return res.status(200).json("token deleted successfully");
-    } catch (err) {
-      return res.status(403).json(err);
-    }
-  }
-);
-//delete in url --> refresh token
-//delete in header -->accessToken
+// router.get("/refresh", async (req, res) => {
+//   const tok = req.headers.token.split(" ")[1];
+//   if (!tok) return res.status(400).json("you are not allowed");
+//   try {
+//     const tic = await refreshdb.findOne({ token: tok });
+//     if (!tic) {
+//       return res.status(403).json("this refresh  token has been stopped ...");
+//     }
+//     jwt.verify(tok, process.env.jwt_refresh, (err, user) => {
+//       if (err) return res.status(403).json("invalid token");
+//       req.user = user;
+//     });
+//     const { iat, ...OTHERS } = req.user;
+//     const newtoken = jwt.sign(OTHERS, process.env.JWT_SEC, { expiresIn: "3d" });
+//     res.status(200).json(newtoken);
+//   } catch (err) {
+//     res.status(403).json("hellooooo");
+//   }
+// });
+// // in refresh
+// // headers ---> refresh token and return newAccessToken
+// router.delete(
+//   "/deleterefresh/:tok",
+//   verifyTokenAndAuthorization,
+//   async (req, res) => {
+//     const tok = req.params.tok;
+//     if (!tok) return res.status(400).json("you are not allowed");
+//     try {
+//       await refreshdb.findOneAndDelete({ token: tok });
+//       return res.status(200).json("token deleted successfully");
+//     } catch (err) {
+//       return res.status(403).json(err);
+//     }
+//   }
+// );
+// //delete in url --> refresh token
+// //delete in header -->accessToken
 module.exports = router;
